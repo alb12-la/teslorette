cf71395752609940f862a417c4f8e0deaa7c3573
alb <alb4251@gmail.com>
2025-05-29 17:00:26 -0700

    save point

---
+++ src/app/itinerary-poster/itinerary-poster.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itinerary-poster',
+  standalone: true,
+  templateUrl: './itinerary-poster.component.html',
+  styleUrls: ['./itinerary-poster.component.scss']
+})
+export class ItineraryPosterComponent {} 
